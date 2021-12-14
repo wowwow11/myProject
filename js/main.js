b8edcf344be02799689b1476c863d76210c345bf
wowwow11 <81003952+wowwow11@users.noreply.github.com>
2021-12-15 08:24:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", ()=>{
+    const grid = new Isotope("section", {
+        itemSelector: "article",
+        columnWidth: "article",
+        transitionDuration: "0.5s"
+    });
+});
